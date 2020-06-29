5157952bf61eac3d0a054e005f0ca7630ded9ecb
RandyBeans448 <65494300+RandyBeans448@users.noreply.github.com>
2020-06-30 00:14:07 +0100

    Add files via upload

---
+++ models/book.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const Router = require('Router');
+const Sequelize = require('sequelize');
+'use strict';
+module.exports = (sequelize) => {
+    class Book extends Sequelize.Model {
+      
+    }
+    Book.init({
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: Sequelize.STRING,
+            allowNull: false, // disallow null
+            validate: {
+              notEmpty: {
+                msg: 'Please provide a value for "title"'
+              }
+             }
+          },
+          author: {
+            type: Sequelize.STRING,
+            allowNull: false, // disallow null
+            validate: {
+              notEmpty: {
+                msg: 'Please provide a value for "author"'
+              }
+             }
+          },
+          genre: {
+            type: Sequelize.STRING,
+            allowNull: false, // disallow null
+            validate: {
+              notEmpty: {
+                msg: 'Please provide a value for "genre"'
+              }
+             }
+          },
+          year: {
+            type: Sequelize.INTEGER,
+            allowNull: false, // disallow null
+            validate: {
+              notEmpty: {
+                msg: 'Please provide a value for "year"'
+              }
+             }
+          }
+       }, { sequelize })
+    return Book
+}
